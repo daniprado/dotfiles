a1f6483e76352192cb341d44aac1fecb5a1b2e35
Daniel Prado <dpradom@argallar.com>
2021-09-10 23:40:09 +0200

    Added _beet autocomplete

--- ncmpcpp/_files/init.sh
+++ ncmpcpp/_files/init.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+BEETCOMP_URL="https://raw.githubusercontent.com/beetbox/beets/master/extra/_beet"
+
 source ${AG_SRC:-~/dotfiles}/script/_files/common.sh
 
 PIP_PKGS=( "python-mpd2" "pylas" "discogs-client" )
 pip_install "${PIP_PKGS[@]}"
 
 pkg_install "ncmpcpp" "mpd ncmpcpp beets"
+
+(cd ${AG_CONFIG}/zsh/completions; \
+  rm _beet || echo "Beets autocomplete not present"; \
+  wget "${BEETCOMP_URL}")
 
 exit 0

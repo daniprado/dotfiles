0084eee01c4e8873c2dba15b0ed9b69937920e3f
Daniel Prado <dpradom@argallar.com>
2022-01-12 10:08:28 +0100

    Another brick in the GPG

--- zsh/.config/zsh/gpg-init.zsh
+++ zsh/.config/zsh/gpg-init.zsh
@@ -2,11 +2,6 @@
 
 if [[ "$(whoami)" == "$(logname)" ]]; then
   if [[ ! "${SSH_TTY}" ]]; then
-
-    GPG_UID=$(id -u)
-    GPG_AGENT_PID=$(pgrep --uid ${GPG_UID} gpg-agent)
-    GPG_AGENT_TMP="/run/user/${GPG_UID}/gnupg"
-
     export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
     # export SSH_AUTH_SOCK="${GPG_AGENT_TMP}/S.gpg-agent.ssh"
   fi

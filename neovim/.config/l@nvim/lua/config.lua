3b1911ba60086ea5dd432d2707c4dd446b7ee307
Daniel Prado <dpradom@argallar.com>
2022-05-14 19:01:25 +0200

    LSP Install changes

--- neovim/.config/l@nvim/lua/config.lua
+++ neovim/.config/l@nvim/lua/config.lua
@@ -194,17 +194,20 @@
 -- }}}
 
 -- LSP {{{
+  local servers = { "vimls", "sumneko_lua", "pylsp", "pyright", "sourcery", "jedi_language_server", "bashls", "dockerls", "jsonls", "terraformls", "yamlls" }
+
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   local lsp_installer = require("nvim-lsp-installer")
-  lsp_installer.settings({
+  lsp_installer.setup({
+    ensure_installed = servers,
+    automatic_installation = true,
     ui = { icons = { server_installed = "✓", server_pending = "➜", server_uninstalled = "✗" } }
   })
-  lsp_installer.on_server_ready(function(server)
-    local opts = {
-      capabilities = capabilities
-    }
-    server:setup(opts)
-  end)
+
+  local lspconfig = require("lspconfig")
+  for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup { capabilities = capabilities }
+  end
 -- }}}
 
 -- CMP {{{
